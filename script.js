a83da264e62ded9f1c46172730512e32fcca8759
lekyks16 <mes.sites.web@gmail.com>
2026-07-16 11:00:08 +0200

    boutton nouvel élément presque fini

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-let modal = document.getElementById("modal");
+// Boutton Sauvegarder
+let modalSauvegarder = document.getElementById("modalSauvegarder");
 let sauvegarder = document.getElementById("sauvegarder");
-let annulerModal = document.getElementById("annulerModal");
-let confirmer = document.getElementById("confirmer");
+let annulerSauvegarder = document.getElementById("annulerSauvegarder");
+let confirmerSauvegarder = document.getElementById("confirmerSauvegarder");
 
 sauvegarder.addEventListener("click", function() {
-    modal.style.display = "flex";
+    modalSauvegarder.style.display = "flex";
 });
-annulerModal.addEventListener("click", function() {
-    modal.style.display = "none";
+annulerSauvegarder.addEventListener("click", function() {
+    modalSauvegarder.style.display = "none";
 });
-confirmer.addEventListener("click", function() {
-    modal.style.display = "none";
+confirmerSauvegarder.addEventListener("click", function() {
+    modalSauvegarder.style.display = "none";
 });
+
+
+// Boutton Nouvel élément
+let modalNouvelElement = document.getElementById("modalNouvelElement");
+let nouvelElement = document.getElementById("nouvelElement");
+let annulerNouvelElement = document.getElementById("annulerNouvelElement");
+let creerNouvelElement = document.getElementById("creerNouvelElement");
+nouvelElement.addEventListener("click", function() {
+    modalNouvelElement.style.display = "flex";
+});
+annulerNouvelElement.addEventListener("click", function() {
+    modalNouvelElement.style.display = "none";
+});
+creerNouvelElement.addEventListener("click", function() {
+    modalNouvelElement.style.display = "none";
+});
+
+
+// Boutton Annuler
+
+
+
+// Bouton Refaire
